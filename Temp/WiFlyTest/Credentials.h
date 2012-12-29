22fd85ff6e48c5ff32d8a659638ffd7c9e7807da
rolfwessels <rolf.wessels@gmail.com>
2012-12-29 18:32:06 +0200

    Initial commit

---
+++ Temp/WiFlyTest/Credentials.h
@@ -0,0 +1,9 @@
+#ifndef __CREDENTIALS_H__
+#define __CREDENTIALS_H__
+
+// Wifi parameters
+char passphrase[] = "sogaanditmaar";
+char ssid[] = "CC902";
+char ntp_server[] = "129.6.15.28 "; // or your favorite ntp server.
+
+#endif
